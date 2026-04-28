e73b58665f1c28fb9c8a7de80336a3e8f9449b61
Leandro <leandroriquelme91@gmail.com>
2026-04-28 16:19:46 -0400

    limpiar repositorio

--- Ejercicios.py
+++
@@ -1,5 +0,0 @@
-email = input("Ingrese su correo electrónico: ").strip().lower()
-if "@" in email:
-    print(f"Correo electrónico ingresado: {email}")
-else:
-    print("Correo electrónico inválido.")
--- Funciones.py
+++
@@ -1,7 +0,0 @@
-def menu():
-    print("- Calculadora -")
-    print("1.- Sumar")
-    print("2.- Restar")
-    print("3.- Multiplicar")
-    print("4.- Dividir")
-    print("5.- Potencia")
